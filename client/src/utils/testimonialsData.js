b2f37c386d8e6e02399d2df24d8b690242d7d639
CalebMunene254 <calebmunene67@gmail.com>
2025-07-03 10:35:54 +0300

    First

--- client/src/utils/testimonialsData.js
+++ client/src/utils/testimonialsData.js
@@ -1,20 +1,20 @@
 export const testimonials = [
     {
-        name:"Sarah Johnson",
+        name:"Sarah ",
         job: "Yoga Instructor",
         review: "MindWell has completely transformed my approach to mental wellness. The guided meditations have helped me manage stress and anxiety effectively",
         shorthand: "SJ"
     },
 
     {
-        name:"Michael Rodriguez",
+        name:"Michael Kimani",
         job: "Software Engineer",
         review: "The sleep stories and calming exercises have significantly improved my sleep quality. I wake up feeling refreshed and energized.",
         shorthand: "MR"
     },
 
     {
-        name:"Emma Parker",
+        name:"Shirley Mwende",
         job: "Student",
         review: "The daily mood tracking and journal prompts have helped me understand my emotions better. I feel more in control of my mental health.",
         shorthand: "EP"
